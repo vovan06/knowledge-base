2c89fde6ce0e7da32d0ab25a69f80af691d6f76f
vovan06 <barashewova@gmail.com>
2022-12-16 21:15:18 +0300

    api

--- server/authsystem/views.py
+++ server/authsystem/views.py
@@ -1,3 +1,39 @@
+from django.forms import model_to_dict
+from rest_framework import generics
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import UserManager
+from .models import User
+from .serializers import CatalogSerializer
 
-# Create your views here.
+
+class UserManagerAPIView(APIView):
+    def get(self, request):
+        lst = UserManager.objects.all().values()
+        return Response({'posts': list(lst)})
+
+    def post(self, request):
+        post_new = UserManager.objects.create(
+            user=request.data['user']
+        )
+        return Response({'post': model_to_dict(post_new)})
+
+class UserAPIView(APIView):
+    def get(self, request):
+        lst = User.objects.all().values()
+        return Response({'posts': list(lst)})
+
+    def post(self, request):
+        post_new = User.objects.create(
+            name=request.data['name'],
+            middlename=request.data['middlename'],
+            surname=request.data['surname'],
+            department=request.data['department'],
+            email=request.data['email'],
+            private_access=request.data['private_access'],
+            is_active=request.data['is_active'],
+            is_staff=request.data['is_staff'],
+            is_superuser=request.data['user']
+        )
+        return Response({'post': model_to_dict(post_new)})
